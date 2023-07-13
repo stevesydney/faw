1612f5bb95c5d8637a0460dee3b76f2ce4b29578
Steve Farrugia <sfarrugia@gmail.com>
2023-07-13 18:54:58 +0200

    Updated footer links to include github and new bluesky and also removed webmentions css if its there

--- functions.php
+++ functions.php
@@ -208,5 +208,6 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 /*  DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
 function wps_deregister_styles() {
     wp_dequeue_style( 'global-styles' );
+	wp_dequeue_style( 'webmention' );
 }
 add_action( 'wp_enqueue_scripts', 'wps_deregister_styles', 100 );
