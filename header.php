86ef4239f51fc95687b199ba0c3350b15d612548
Steve Farrugia <sfarrugia@gmail.com>
2023-07-19 10:21:48 +0200

    Added Matomo analytics tag

--- header.php
+++ header.php
@@ -26,6 +26,21 @@
 <meta name="description" content="<?php bloginfo( 'description' )?>">
 <?php endif; ?>
 <?php wp_head(); ?>
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.fasterandworse.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 </head>
 
 <body <?php body_class(); ?>>
