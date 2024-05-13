457494fc14ffb1157f7bc67cbb4c5cc4a5f881d4
Steve Farrugia <sfarrugia@gmail.com>
2024-05-13 18:48:17 +0200

    removed substack

--- footer.php
+++ footer.php
@@ -13,12 +13,11 @@
 
 	<footer class="global-footer">
 		<ul class="global-footer-links">
-			<li class="global-footer-link hide-visual"><a rel="me" href="https://github.com/stevesydney">GitHub</a></li>
-			<li class="global-footer-link"><a rel="me" href="https://substack.com/@fasterandworse">Substack</a></li>
-			<li class="global-footer-link"><a rel="me" href="https://www.twitch.tv/fasterandworse">Twitch</a></li>
 			<li class="global-footer-link"><a rel="me" href="https://twitter.com/fasterandworse">Twitter</a></li>
 			<li class="global-footer-link"><a rel="me" href="https://hci.social/@fasterandworse">Mastodon</a></li>
 			<li class="global-footer-link"><a rel="me" href="https://bsky.app/profile/fasterandworse.com">Bluesky</a></li>
+			<li class="global-footer-link"><a rel="me" href="https://www.twitch.tv/fasterandworse">Twitch</a></li>
+			<li class="global-footer-link hide-visual"><a rel="me" href="https://github.com/stevesydney">GitHub</a></li>
 		</ul>
 
 		<button type="button" class="global-theme-toggle-button stock" id="theme-toggle-button" aria-label="Change to dark"></button>
